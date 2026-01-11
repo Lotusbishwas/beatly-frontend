77e7f3b46dcacc4e9117a46156bcb5f08d144de5
Lotusbishwas <lotusbishwas321@gmail.com>
2026-01-11 01:27:12 +0000

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import './styles/global.css';
